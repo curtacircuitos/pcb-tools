e565624b8181ea0a9dd5ea1585025a4eec72ac18
Hamilton Kibbe <ham@hamiltonkib.be>
2014-10-06 11:50:38 -0400

    Fix import error

--- gerber/render/render.py
+++ gerber/render/render.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ..statements import (
+from ..gerber_statements import (
     CommentStmt, UnknownStmt, EofStmt, ParamStmt, CoordStmt, ApertureStmt
 )
 
