d90da4000f3fd542da1896e705d3db43fd48ea4b
Hamilton Kibbe <ham@hamiltonkib.be>
2014-10-16 18:13:43 -0400

    Add primitive definitions and bounding box calcs for DRC

---
+++ gerber/primitives.py
@@ -0,0 +1,193 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# copyright 2014 Hamilton Kibbe <ham@hamiltonkib.be>
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import math
+from operator import sub
+
+
+class Primitive(object):
+    def bounding_box(self):
+        """ Calculate bounding box
+
+        will be helpful for sweep & prune during DRC clearance checks.
+        
+        Return ((min x, max x), (min y, max y))
+        """
+        pass
+
+
+class Line(Primitive):
+    """
+    """
+    def __init__(self, start, end, width):
+        self.start = start
+        self.end = end
+        self.width = width
+        
+    @property
+    def angle(self):
+        dx, dy = tuple(map(sub, end, start))
+        angle = degrees(math.tan(dy/dx))
+
+    def bounding_box(self):
+        width_2 = self.width / 2.
+        min_x = min(self.start[0], self.end[0]) - width_2
+        max_x = max(self.start[0], self.end[0]) + width_2
+        min_y = min(self.start[1], self.end[1]) - width_2
+        max_y = max(self.start[1], self.end[1]) + width_2
+        return ((min_x, max_x), (min_y, max_y))
+
+
+class Arc(Primitive):
+    """
+    """
+    def __init__(self, start, end, center, direction, width):
+        self.start = start
+        self.end = end
+        self.center = center
+        self.direction = direction
+        self.width = width
+
+    @property
+    def start_angle(self):
+        dy, dx = map(sub, self.start, self.center)
+        return math.atan2(dy, dx)
+
+    @property
+    def end_angle(self):
+        dy, dx = map(sub, self.end, self.center)
+        return math.atan2(dy, dx)
+
+    def bounding_box(self):
+        pass
+
+class Circle(Primitive):
+    """
+    """
+    def __init__(self, position, diameter):
+        self.position = position
+        self.diameter = diameter
+        self.radius = diameter / 2.
+
+    def bounding_box(self):
+        min_x = self.position[0] - self.radius
+        max_x = self.position[0] + self.radius
+        min_y = self.position[1] - self.radius
+        max_y = self.position[1] + self.radius
+        return ((min_x, max_x), (min_y, max_y))
+
+
+class Rectangle(Primitive):
+    """
+    """
+    def __init__(self, position, width, height):
+        self.position = position
+        self.width = width
+        self.height = height
+
+    @property
+    def lower_left(self):
+        return (self.position[0] - (self.width / 2.), 
+                self.position[1] - (self.height / 2.))
+
+    @property
+    def upper_right(self):
+        return (self.position[0] + (self.width / 2.), 
+                self.position[1] + (self.height / 2.))
+
+    def bounding_box(self):
+        min_x = self.lower_left[0]
+        max_x = self.upper_right[0]
+        min_y = self.lower_left[1]
+        max_y = self.upper_right[1]
+        return ((min_x, max_x), (min_y, max_y))
+
+
+class Obround(Primitive):
+    """
+    """
+    def __init__(self, position, width, height)
+        self.position = position
+        self.width = width
+        self.height = height
+
+    @property
+    def orientation(self):
+        return 'vertical' if self.height > self.width else 'horizontal'
+
+    @property
+    def lower_left(self):
+        return (self.position[0] - (self.width / 2.), 
+                self.position[1] - (self.height / 2.))
+
+    @property
+    def upper_right(self):
+        return (self.position[0] + (self.width / 2.), 
+                self.position[1] + (self.height / 2.))
+
+    def bounding_box(self):
+        min_x = self.lower_left[0]
+        max_x = self.upper_right[0]
+        min_y = self.lower_left[1]
+        max_y = self.upper_right[1]
+        return ((min_x, max_x), (min_y, max_y))
+
+
+class Polygon(Primitive):
+    """
+    """
+    def __init__(self, position, sides, radius):
+        self.position = position
+        self.sides = sides
+        self.radius = radius
+        
+    def bounding_box(self):
+        min_x = self.position[0] - self.radius
+        max_x = self.position[0] + self.radius
+        min_y = self.position[1] - self.radius
+        max_y = self.position[1] + self.radius
+        return ((min_x, max_x), (min_y, max_y))
+
+
+class Region(Primitive):
+    """
+    """
+    def __init__(self, points):
+        self.points = points
+        
+    def bounding_box(self):
+        x_list, y_list = zip(*self.points)
+        min_x = min(x_list)
+        max_x = max(x_list)
+        min_y = min(y_list)
+        max_y = max(y_list)
+        return ((min_x, max_x), (min_y, max_y))
+
+
+class Drill(Primitive):
+    """
+    """
+    def __init__(self, position, diameter):
+        self.position = position
+        self.diameter = diameter
+        self.radius = diameter / 2.
+        
+    def bounding_box(self):
+        min_x = self.position[0] - self.radius
+        max_x = self.position[0] + self.radius
+        min_y = self.position[1] - self.radius
+        max_y = self.position[1] + self.radius
+        return ((min_x, max_x), (min_y, max_y))
