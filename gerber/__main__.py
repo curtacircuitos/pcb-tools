ed7d9ceb349f405ca40f43835a5fd6fc1beed98b
Philipp Klaus <philipp.klaus@gmail.com>
2015-02-18 15:57:49 +0100

    accidentially changed import order in 7ace94b

--- gerber/__main__.py
+++ gerber/__main__.py
@@ -16,9 +16,9 @@
 # the License.
 
 if __name__ == '__main__':
-    import sys
     from gerber.common import read
     from gerber.render import GerberSvgContext
+    import sys
 
     if len(sys.argv) < 2:
         print >> sys.stderr, "Usage: python -m gerber <filename> <filename>..."
