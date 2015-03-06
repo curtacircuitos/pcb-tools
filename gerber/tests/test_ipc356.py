45372cfff3d228851e546a2603496db1e499f86b
Hamilton Kibbe <ham@hamiltonkib.be>
2015-03-06 17:00:40 -0500

    fix tests

--- gerber/tests/test_ipc356.py
+++ gerber/tests/test_ipc356.py
@@ -24,7 +24,8 @@ def test_parser():
     assert_equal(len(ipcfile.components), 21)
     assert_equal(len(ipcfile.vias), 14)
     assert_equal(ipcfile.test_records[-1].net_name, 'A_REALLY_LONG_NET_NAME')
-    assert_equal(set(ipcfile.board_outline),
+    assert_equal(ipcfile.outlines[0].type, 'BOARD_EDGE')
+    assert_equal(set(ipcfile.outlines[0].points),
                  {(0., 0.), (2.25, 0.), (2.25, 1.5), (0., 1.5), (0.13, 0.024)})
 
 def test_comment():
@@ -50,12 +51,15 @@ def test_eof():
     assert_equal(e.to_netlist(), '999')
     assert_equal(str(e), '<IPC-D-356 EOF>')
 
-def test_board_edge():
+def test_outline():
+    type = 'BOARD_EDGE'
     points = [(0.01, 0.01), (2., 2.), (4., 2.), (4., 6.)]
-    b = IPC356_BoardEdge(points)
+    b = IPC356_Outline(type, points)
+    assert_equal(b.type, type)
     assert_equal(b.points, points)
-    b = IPC356_BoardEdge.from_line('389BOARD_EDGE         X100Y100 X20000Y20000'
+    b = IPC356_Outline.from_line('389BOARD_EDGE         X100Y100 X20000Y20000'
                                    ' X40000 Y60000', FileSettings(units='inch'))
+    assert_equal(b.type, 'BOARD_EDGE')
     assert_equal(b.points, points)
 
 def test_test_record():
@@ -71,14 +75,14 @@ def test_test_record():
     assert_almost_equal(r.x_coord, 0.6647)
     assert_almost_equal(r.y_coord, 1.29)
     assert_equal(r.rect_x, 0.)
-    assert_equal(r.soldermask_info, '3')
+    assert_equal(r.soldermask_info, 'both')
     r = IPC356_TestRecord.from_line(record_string, FileSettings(units='metric'))
     assert_almost_equal(r.hole_diameter, 0.15)
     assert_almost_equal(r.x_coord, 6.647)
     assert_almost_equal(r.y_coord, 12.9)
     assert_equal(r.rect_x, 0.)
     assert_equal(str(r),
-                 '<IPC-D-356 Test Record: Net: +5VDC Type: through-hole>')
+                 '<IPC-D-356 +5VDC Test Record: through-hole>')
 
     record_string = '327+3.3VDC          R40   -1         PA01X 032100Y 007124X0236Y0315R180 S0'
     r = IPC356_TestRecord.from_line(record_string, FileSettings(units='inch'))
@@ -93,7 +97,7 @@ def test_test_record():
     assert_almost_equal(r.rect_x, 0.0236)
     assert_almost_equal(r.rect_y, 0.0315)
     assert_equal(r.rect_rotation, 180)
-    assert_equal(r.soldermask_info, '0')
+    assert_equal(r.soldermask_info, 'none')
     r = IPC356_TestRecord.from_line(record_string, FileSettings(units='metric'))
     assert_almost_equal(r.x_coord, 32.1)
     assert_almost_equal(r.y_coord, 7.124)
@@ -101,7 +105,7 @@ def test_test_record():
     assert_almost_equal(r.rect_y, 0.315)
 
 
-    record_string = '317                 J4    -M2   D0330PA00X 012447Y 008030X0000          S0'
+    record_string = '317                 J4    -M2   D0330PA00X 012447Y 008030X0000          S1'
     r = IPC356_TestRecord.from_line(record_string, FileSettings(units='inch'))
     assert_equal(r.feature_type, 'through-hole')
     assert_equal(r.id, 'J4')
@@ -112,5 +116,5 @@ def test_test_record():
     assert_almost_equal(r.x_coord, 1.2447)
     assert_almost_equal(r.y_coord, 0.8030)
     assert_almost_equal(r.rect_x, 0.)
-    assert_equal(r.soldermask_info, '0')
+    assert_equal(r.soldermask_info, 'primary side')
 
